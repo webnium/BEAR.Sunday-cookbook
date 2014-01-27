697bf1578ebda625771b22d89bc1ede0d65d2f77
MugeSo <muges@mugeso.com>
2014-01-27 14:14:50 +0900

    Change nginx conf file path
    Now, install conf file into /etc/nginx/sites-available/<site-name>

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -9,7 +9,7 @@
 include_recipe "nginx"
 include_recipe "php-fpm"
 
-bearsunday_nginx_config "/etc/nginx/conf.d/default.conf" do
+bearsunday_nginx_config "/etc/nginx/sites-available/#{node[:bearsunday][:nginx_site]}" do
   application_path node[:bearsunday][:app][:install_path]
   context           node['bearsunday']['nginx_context']
   listen            80
@@ -22,4 +22,8 @@ bearsunday_nginx_config "/etc/nginx/conf.d/default.conf" do
   notifies :reload, "service[nginx]", :delayed
   notifies :reload, "service[php-fpm]", :delayed
   subscribes :reload, "bash[install_bear_package]", :immediately if %w{Helloworld Sandbox}.include?(node['bearsunday']['app_name'])
+end
+
+nginx_site node[:bearsunday][:nginx_site] do
+  enable node[:bearsunday][:nginx_site_enabled]
 end
